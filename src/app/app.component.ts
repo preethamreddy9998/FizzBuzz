c7621ecc3633be49a53204bc1a6cfc08936419c5
preethamreddy9998 <77567021+preethamreddy9998@users.noreply.github.com>
2021-01-18 01:22:53 -0500

    Updated the logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   public outputLabel = environment.outputLabel;
 
   /** There are the input values (Collection of strings/numbers/invaild ) */
-  public stringCollection = [1, 3, 5, , 15, "A", 23];
+  public stringCollection = [1, 3, 5, , 15, "A", 23, 97];
 
   /** Factor and its respected return values */
   public mappings = [{ factor: 3, word: "Fizz" }, { factor: 5, word: "Buzz" }];
@@ -34,28 +34,21 @@ export class AppComponent {
     if (words.length > 0) {
       return words.join("");
     } else {
-      return "Divided " + number.toString() + " by 3 and 5";
+      return (
+        "Divided " +
+        number.toString() +
+        " by 3 <br /> Divided " +
+        number.toString() +
+        " by 5"
+      );
     }
   }
 
   multipleOf(factor, number) {
     return number % factor === 0;
   }
-  // getTheOutput(searchValue: any): string {
-  //   if (!Number(searchValue)) {
-  //     return "Invalid Item";
-  //   } else if (parseInt(searchValue) % 15 === 0) {
-  //     return "FizzBuzz\t";
-  //   } else if (parseInt(searchValue) % 3 === 0) {
-  //     return "Fizz\t";
-  //   } else if (parseInt(searchValue) % 5 === 0) {
-  //     return "Buzz\t";
-  //   } else {
-  //     return searchValue;
-  //   }
-  // }
 
-  /** we have another way of doing this task. Uncomment the below code if you want to provide customized inputs from UI. so that it will takes the input from input box and display the corresponding vlaue on the screen. Dont forget to Uncomment the corresponding HTML code*/
+  /** we have another way of doing this task. Uncomment the below code if you want to provide customized inputs from UI. so that it will take the input from input box and display the corresponding vlaues on the screen. Dont forget to Uncomment the corresponding HTML code*/
 
   // public inputValue;
   // public outputValue;
